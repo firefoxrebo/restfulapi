87367354799590fb55ff4c8dbc7841f0106f06da
Mohammed Yehia <engmohammedyehia@hotmail.com>
2018-01-13 01:58:47 +0300

    DELETED OLD PROJECT

--- Quiries/init.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-require_once realpath(dirname(__FILE__)) . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'Config' . DIRECTORY_SEPARATOR . 'main.php';
-require_once APP_PATH . 'Helpers' . DIRECTORY_SEPARATOR . 'autoload.php';
